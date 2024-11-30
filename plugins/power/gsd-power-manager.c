068aa39e164722c017e93c774874d487fb182e55
Jesús Higueras <jesus@furilabs.com>
2024-11-30 12:57:40 +0100

    power: update saved_brightness value on any brightness change

--- plugins/power/gsd-power-manager.c
+++ plugins/power/gsd-power-manager.c
@@ -2902,6 +2902,7 @@ iio_proxy_changed (GsdPowerManager *manager)
 
          /* Apply brightness directly, hardware knows what to set */
         if (manager->linear_brightness_ambient_unit == GSD_POWER_LIGHT_LEVEL_UNIT_VENDOR) {
+                manager->saved_brightness = manager->linear_brightness_ambient_iio;
                 gsd_backlight_set_brightness_async (manager->backlight,
                                                     manager->linear_brightness_ambient_iio,
                                                     NULL, NULL, NULL);
@@ -3701,6 +3702,9 @@ handle_set_property_other (GsdPowerManager *manager,
                 g_variant_get (value, "i", &brightness_value);
                 if (manager->backlight) {
                         handle_user_brightness (manager, brightness_value);
+
+                        manager->saved_brightness = brightness_value;
+
                         gsd_backlight_set_brightness_async (manager->backlight, brightness_value,
                                                             NULL, NULL, NULL);
                         return TRUE;
